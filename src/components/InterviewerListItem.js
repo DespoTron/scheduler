5ecfc01b5e9d586c49e3b34f1e1ec5669a97f89a
Thai Do <thai.hl.do@gmail.com>
2020-09-29 14:42:18 -0700

    Fixed the import function not importing the scss file

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,23 +1,18 @@
 import React from 'react';
 
-import "components/InterviewerListItem";
+import './InterviewerListItem.scss';
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const interviewerClass = classNames("interviewers__item", {
-      "interviewers__item--selected" : props.selected
-   });
-
-return ( 
-  <li 
-    className={interviewerClass} onClick={props.setInterviewer}
-  >
-    <img
-      className={"interviewers__item-image"}
-      src={props.avatar}
-      alt={props.name} 
-    />
+  const className = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
+  return (
+    <li className={className} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt="Sylvia Palmer"
+      />
       {props.selected && props.name}
-  </li>
-);
+    </li>
+  );
 };
