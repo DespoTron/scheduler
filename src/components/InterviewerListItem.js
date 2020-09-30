4e108852d5834975e659a60e25c6910d791f80d1
Thai Do <thai.hl.do@gmail.com>
2020-09-29 19:29:46 -0700

    Finished off Form component and fixed type in stores/index.js where there was a misspelling and could not use the method map to iterate over the array

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,15 +4,15 @@ import './InterviewerListItem.scss';
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const className = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
+  const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
   return (
-    <li className={className} onClick={props.setInterviewer}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
-        alt="Sylvia Palmer"
+        alt={props.name}
       />
       {props.selected && props.name}
     </li>
   );
-};
+}
