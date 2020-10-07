016a69692162fdfc194d20bc28ffe66020dae44b
Thai Do <thai.hl.do@gmail.com>
2020-10-07 14:47:53 -0700

    Got Edit Interview Test to work

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -41,26 +41,38 @@ describe("Appointments", () => {
 
   it("should edit an interview", () => {
     cy.get("[alt=Add]")
-      .first()
-      .click();    
-
+    .first()
+    .click();    
+    
     cy.get("[data-testid=student-name-input]")
-      .type("Lydia Miller-Jones");
-
+    .type("Lydia Miller-Jones");
+    
     cy.get("[alt='Sylvia Palmer']")
-      .click();
+    .click();
     
     cy.contains("Save")
-      .click();
-
+    .click();
+    
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
     
-    cy.get(".appointment__card--show")
-      .first()
-      .trigger("mouseover")
+    cy.get("[alt=Edit]").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");    
     
-    cy.get(".appointment__")
+
+    // Clicks the edit button for the existing appointment
+    // Changes the name and interviewer
+    // Clicks the save button
+    // Sees the edit to the appointment    
   });
 
 });
