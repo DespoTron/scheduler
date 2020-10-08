f02f955f40ae8b1e46547cbec396cf2e1e609229
Thai Do <thai.hl.do@gmail.com>
2020-10-07 18:05:06 -0700

    Cleaning up code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -20,23 +20,6 @@ export default function useVisualMode(initialmode) {
       setHistory(prev => [...prev, newmode])
     }
   };
-    
-    // replace ? setHistory(prev => [...prev.slice(0, -1), newmode]) : setHistory(prev => [...prev, newmode])
-    // if (replace) {
-
-    //   history[history.length - 1] = newmode
-    //   setMode(newmode);
-    //   // history.pop();
-    //   return;
-    // }
-
-    // // update the mode state with the new value
-
-    // // When transition is called, we need to add the new mode to our historyy
-    // // history.push(newmode)
-    // setHistory(prev => ([...prev, newmode]));
-
-    // setMode(newmode)
 
     const back = () => {
       if (history.length < 2) return;
@@ -45,22 +28,4 @@ export default function useVisualMode(initialmode) {
 
     return { mode: history[history.length - 1], transition, back };
 };
-  
-  // When back is called, we should set the mode to the previous item in our history array
-  // const back = () => {
 
-  //   // Constraint on our back function/should not allow the user to go back past initial mode
-  //   // This means that our history array will always need to have a length that is great than 1
-  //   if (history.length > 1) {
-  //     // setMode(mode)
-  //     // history.pop();
-
-  //     setHistory(history);
-
-  //     setMode(history[history.length - 2]);
-  //   } 
-  // }
-  
-
-  // // return an object with a mode property
-  // return { mode, transition, back }
